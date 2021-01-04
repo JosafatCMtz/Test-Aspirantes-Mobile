fc6c4f42cbd0390789441b1bd42ccdcf8bf3ab82
JosafatiOSDeveloper <josamtz@outlook.com>
2021-01-04 11:17:07 -0600

    Initial Commit

---
+++ Test_Aspirantes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Aspirantes
+//
+//  Created by Josafat Martínez  on 04/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
